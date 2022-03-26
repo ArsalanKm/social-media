198218a5731b885a6ecbf359e9230261554eb266
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-03-26 12:24:19 +0430

    user Model created

--- api/index.js
+++ api/index.js
@@ -6,12 +6,16 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const { allowedNodeEnvironmentFlags } = require("process");
+
 const PORT = 8080;
 const client = mongodb.MongoClient;
 
 dotenv.config();
 // db is service name
-client.connect("mongodb://db:27017/social-media", (err, db) => {
+mongoose.connect("mongodb://db:27017/social-media", (err, db) => {
   if (err) {
     console.log("database is not connected");
   } else {
@@ -19,9 +23,14 @@ client.connect("mongodb://db:27017/social-media", (err, db) => {
   }
 });
 
-app.get("/", (req, res) => {
-  res.json({ hello: "how are today or not " });
-});
+// middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+// routers
+app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.listen(PORT, () => {
   console.log("listening on port 8080");
