9adb9ff366a59c62230ed24d0b1095f26ca221c0
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-03-26 15:46:01 +0430

    bcrypt problem remain

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,14 +1,21 @@
 const router = require("express").Router();
 const User = require("../models/User");
+// const bcrypt = require("bcrypt");
 
 // register
 router.post("/register", async (req, res) => {
-  const newUser = await new User({
-    username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  });
   try {
+    const salt = await bcrypt.genSalt(10);
+    // const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    const {
+      body: { username, email, password },
+    } = req;
+    const newUser = new User({
+      username,
+      email,
+      password,
+    });
+
     const user = await newUser.save();
     res.status(200).json(user);
   } catch (err) {
